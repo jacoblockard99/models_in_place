18c090917fecc6a1d5c474d2d5b0c63b419f098b
Jacob <jacoblockard99@gmail.com>
2021-07-15 21:08:19 -0400

    Set up up basic CI
    Set up Travis CI, Inch CI, and CodeClimate, and add the appropriate
    badges to the README. Then setup up RuboCop and make the necessary
    changes to adhere to its style. Also correct the .gitignore and edit gem
    metadata.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # ModelsInPlace
+
+[![Build Status](https://www.travis-ci.com/jacoblockard99/models_in_place.svg?branch=master)](https://www.travis-ci.com/jacoblockard99/models_in_place)
+[![Inline docs](http://inch-ci.org/github/jacoblockard99/models_in_place.svg?branch=master)](http://inch-ci.org/github/jacoblockard99/models_in_place)
+[![Maintainability](https://api.codeclimate.com/v1/badges/e94de688acfc28dd569e/maintainability)](https://codeclimate.com/github/jacoblockard99/models_in_place/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/e94de688acfc28dd569e/test_coverage)](https://codeclimate.com/github/jacoblockard99/models_in_place/test_coverage)
+
 Short description and motivation.
 
 ## Usage
--- lib/models_in_place.rb
+++ lib/models_in_place.rb
@@ -1,6 +1,24 @@
-require "models_in_place/version"
-require "models_in_place/railtie"
+# frozen_string_literal: true
 
+require 'models_in_place/version'
+require 'models_in_place/railtie'
+
+# {ModelsInPlace} is the top-level namespace that contains all classes and modules within the
+# models_in_place gem.
+#
+# @author Jacob Lockard
+# @since 0.1.0
 module ModelsInPlace
-  # Your code goes here...
+  # {Error} is a subclass of {https://ruby-doc.org/core-2.5.0/StandardError.html StandardError}
+  # from which all custom errors in models_in_place are derived. One potential use for this class
+  # is to rescue all custom errors produced by models_in_place. For example:
+  #
+  #   begin
+  #     # Do something risky with models_in_place here...
+  #   rescue Middlegem::Error
+  #     # Catch any models_in_place-specific error here...
+  #   end
+  #
+  # @see https://ruby-doc.org/core-2.0.0/Exception.html
+  class Error < StandardError; end
 end
