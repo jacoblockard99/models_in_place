f057c4c04002010d87b76036dca0885cfb854dd9
Jacob <jacoblockard99@gmail.com>
2021-07-19 21:12:38 -0400

    Update files for edit_in_place 0.2.0
    Update OptionsInjector, OptionsInsert, and SignatureEnforcer to accept
    the mode, rather than the field options, since that is now the case in
    edit_in_place 0.2.0.

--- lib/models_in_place/middlewares/signature_enforcer.rb
+++ lib/models_in_place/middlewares/signature_enforcer.rb
@@ -15,20 +15,19 @@ module ModelsInPlace
     class SignatureEnforcer < Middlegem::Middleware
       # Executes the {SignatureEnforcer} middleware by accepting only the arguments it expects
       # and ensuring that the options argument is a hash.
-      # @param field_options [FieldOptions] the {FieldOptions} instance containing the options
-      #   and context required to render the field.
+      # @param mode [Symbol] the mode in which the field should be rendered.
       # @param model [Object] the model instance for which to render the field.
       # @param attribute [String, Symbol] the name of the attribute for which to render the
       #   field.
       # @param options [Hash] the hash of options to the field type.
       # @return [Array] the valid arguments.
       # @raise [InvalidFieldSignatureError] if the field input is invalid.
-      def call(field_options, model, attribute, options)
+      def call(mode, model, attribute, options)
         unless options.is_a? Hash
           raise InvalidFieldSignatureError, 'The options argument is not a hash!'
         end
 
-        [field_options, model, attribute, options]
+        [mode, model, attribute, options]
       end
     end
   end
